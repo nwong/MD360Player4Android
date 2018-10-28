330f42e209e3cb87b4ef3f26495430deaaac5700
nwong <nwong@mylocalappstore.com>
2018-10-28 03:22:18 -0400

    Updated to version 5.8 (69) with the following changes: ## Features - Added support for FCM push notifications for reports V2 real-time notifications - Unregister from Twilio FCM push notifications on logout - When in the foreground, push notifications will result in an alert presented to the user and the notification is also displayed in the notification tray.  If an existing alert is displayed when a new notification is received, the new notification will be displayed in an alert. - When in the background, notifications shall be displayed in the notification tray.  When clicked, notifications shall be removed from the tray. ## Maintenance - Upgraded to use AndroidX support library https://developer.android.com/jetpack/androidx/ - Updated to use cradle 3.2.1 - Updated `compileSdkVersion` to 28 ## Bug Fixes - Fixed issue with unelectable histogram range buttons

--- app/src/main/java/com/asha/md360player4android/CubemapPlayerActivity.java
+++ app/src/main/java/com/asha/md360player4android/CubemapPlayerActivity.java
@@ -6,7 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.DrawableRes;
+import androidx.annotation.DrawableRes;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
